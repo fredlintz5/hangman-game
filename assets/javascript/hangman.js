7fea1ab3d8082aeb10e23c3c2e0d9e40073a5fd7
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-07-28 23:49:45 -0600

    final js tweaks, added bootstrap, css styling, and added comments to js

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,9 +1,8 @@
 window.onload = function() {
 
-var words = ["apple", "banana", "orange", "grape", "strawberry", "blueberry"];
-var wins = 0;
+var words = ["apple", "banana", "orange", "grape", "strawberry", "blueberry", "lemon"];
 var guessesRemaining = 7;
-
+//generates random #, and assigns it to an item in the array, storing it in randomWord
 var randomWord = words[Math.floor((Math.random() * words.length))];
 var underScoreWord = "";
 var letterGuess = [];
@@ -11,57 +10,73 @@ var indexes = [];
 var trueFalse = false;
 
 
+//call underscore function to display randomWord underscores
 underScores();
-    
 
+//listen for keypress, and run function
 document.addEventListener('keypress', function(event) {
 
+    //pushes key pressed into array of guessed letters
     letterGuess.push(String.fromCharCode(event.keyCode));
 
+    //loop through random word's letters, checking against keypress
     for (var j = 0; j < randomWord.length; j++) {
         
+        //if any letter of randomWord is equal to the last letter pressed
         if (randomWord[j] === letterGuess[letterGuess.length-1]) {
+            
+            //tell me keypress matches character in randomWord (used for if/else later)
             trueFalse = true;
+            //tell me at what index the matching characters are at
             indexes.push(j);
+            //change underScoreWord from string to array for looping
             var splitWord = underScoreWord.split("");
 
+            //loop through splitWord array, and set matching indices to corresponding keypress
             for (k = 0; k < indexes.length; k++) {
                 splitWord[indexes[k]] = letterGuess[letterGuess.length-1];
+                //reset indexes array to nothing, so we can loop through again later
                 indexes = [];
+                //rejoin underScoreWord as string for display
                 underScoreWord = splitWord.join("");
             }
         } 
     }
 
+    //if/else statement checking success or failure of matching characters
     if (trueFalse) {
         document.getElementById('randomWord').innerHTML = underScoreWord;
-        document.getElementById('guessed').innerHTML = letterGuess;
+        // If characters match, remove last entered character from letterGuess array
+        // I only want incorrect letters in the letterGuess array
+        letterGuess.pop();
 
 
     } else {
+        // If keypress has no matches, lower guesses by 1
         guessesRemaining--
+        //display letters guessed
         document.getElementById('guessed').innerHTML = letterGuess;
-        document.getElementById('randomWord').innerHTML = underScoreWord;
+        //display remaining chances
         document.getElementById('remainingGuesses').innerHTML = "Guesses Remaining: " + guessesRemaining;
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-6.png height='400'/>";
-
     }
 
+    //reset trueFals to false, to restart entire process
     trueFalse = false;
+    //call function to determine win/lose (see below)
     checkWin();
+    //call function for appropriate image, depending on state of missed guesses
     images();
     
 });
 
 
 
-//converts random word length into string of underscores seperated by spaces
+//converts randomWord into string of underscores of matching length
 function underScores() {
     for (var i = 0; i < randomWord.length; i++) {
         underScoreWord += "_";
     }
     document.getElementById('randomWord').innerHTML = underScoreWord;
-    console.log(randomWord);
 }
 
 //checks to see if player wins or loses, and alerts the answer
@@ -71,41 +86,35 @@ function checkWin() {
         } else if (guessesRemaining === 0) {
             alert("You Lose! Refresh page for new game.");
         }
-
 }   
 
+//calls specific image related to # of guess remaining
 function images(){
     if (guessesRemaining === 6) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-6.png height='400'/>";
-
-    } else if (guessesRemaining === 5) {
         document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-5.png height='400'/>";
 
-    } else if (guessesRemaining === 4) {
+    } else if (guessesRemaining === 5) {
         document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-4.png height='400'/>";
 
-    } else if (guessesRemaining === 3) {
+    } else if (guessesRemaining === 4) {
         document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-3.png height='400'/>";
-        
-    } else if (guessesRemaining === 2) {
+
+    } else if (guessesRemaining === 3) {
         document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-2.png height='400'/>";
         
-    } else if (guessesRemaining === 1) {
+    } else if (guessesRemaining === 2) {
         document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-1.png height='400'/>";
         
-    } else if (guessesRemaining === 0) {
+    } else if (guessesRemaining === 1) {
         document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-0.png height='400'/>";
-        
+
+    } else if (guessesRemaining === 0) {
+        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-00.png height='400'/>";   
     }
-
 }
 
 
 }
-
-
-
-
 
 
 
