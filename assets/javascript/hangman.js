298d01f24b30e1a026745d0f8bad672bbdbd04a4
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-07-29 13:58:58 -0600

    changed image names

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -92,25 +92,25 @@ function checkWin() {
 //calls specific image related to # of guess remaining
 function images(){
     if (guessesRemaining === 6) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-5.png height='400'/>";
+        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-6.png height='400'/>";
 
     } else if (guessesRemaining === 5) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-4.png height='400'/>";
+        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-5.png height='400'/>";
 
     } else if (guessesRemaining === 4) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-3.png height='400'/>";
+        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-4.png height='400'/>";
 
     } else if (guessesRemaining === 3) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-2.png height='400'/>";
+        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-3.png height='400'/>";
         
     } else if (guessesRemaining === 2) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-1.png height='400'/>";
+        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-2.png height='400'/>";
         
     } else if (guessesRemaining === 1) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-0.png height='400'/>";
+        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-1.png height='400'/>";
 
     } else if (guessesRemaining === 0) {
-        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-00.png height='400'/>";   
+        document.getElementById('hangman').innerHTML = "<img src=assets/images/hangman-0.png height='400'/>";   
     }
 }
 
